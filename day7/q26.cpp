83892b35e7f25db4b3b2a6ffd6ec09913ab7b2ec
mayank singh <mayanksinghjadon2007@gmail.com>
2026-06-21 23:32:55 +0530

    day7 assignment is uploaded

---
+++ day7/q26.cpp
@@ -0,0 +1,21 @@
+//wap to recursive fibbonacci series
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int num;
+    cout << "Enter the number of terms: ";
+    cin >> num;
+    cout << "Fibonacci series: ";
+    for (int i = 0; i < num; i++) {
+        cout << fibonacci(i) << " ";
+    }
+    return 0;
+}
